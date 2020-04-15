6982a046ad7f2b23b66db10aaf34c03bab437f79
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2020-04-15 06:00:54 +0530

    updated db server configurations

--- config/db_config.php
+++ config/db_config.php
@@ -1,12 +1,12 @@
 <?php
-    $db_server="localhost:3306";
-    $db_name="foodshala";
-    $db_user_name="root";
-    $db_password="";
-    // $db_server="sql12.freesqldatabase.com:3306";
-    // $db_name="sql12333339";
-    // $db_user_name="sql12333339";
-    // $db_password="3IW5B6uDmc";
+    // $db_server="localhost:3306";
+    // $db_name="foodshala";
+    // $db_user_name="root";
+    // $db_password="";
+    $db_server="sql12.freesqldatabase.com:3306";
+    $db_name="sql12333339";
+    $db_user_name="sql12333339";
+    $db_password="3IW5B6uDmc";
     $conn =  new mysqli($db_server,$db_user_name,$db_password,$db_name);
     if($conn->connect_error)
     {
