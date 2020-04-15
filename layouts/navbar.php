c5294c3d81c8af9f5f72168de1a2be7b6b419e6e
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2020-04-15 06:19:32 +0530

    added meta tags

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -6,6 +6,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#68c3d1">
+    <meta name="author" content="Utkarsh Singh">
+    <meta name="description" content="FoodShala">
+    <meta name="keywords" content="foodshala, utkarsh, utkarsh1999, internshala, utkarsh singh, restaurants">
     <title>FoodShala</title>
     
     <!-- bootstrap links -->
@@ -27,7 +31,7 @@
     <!-- navbar -->
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">FoodShala</a>
+  <a class="navbar-brand" href="index.php">FoodShala</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
